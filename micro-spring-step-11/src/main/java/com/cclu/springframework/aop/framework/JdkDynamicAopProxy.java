d2cd2771cdaa9907adea4893d08c1f129a4d3529
ChangCheng Lu <15252342585@163.com>
2023-08-22 16:38:01 +0800

    micro-spring-step-11: aop

--- micro-spring-step-11/src/main/java/com/cclu/springframework/aop/framework/JdkDynamicAopProxy.java
+++ micro-spring-step-11/src/main/java/com/cclu/springframework/aop/framework/JdkDynamicAopProxy.java
@@ -1,5 +1,12 @@
 package com.cclu.springframework.aop.framework;
 
+import com.cclu.springframework.aop.AdvisedSupport;
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * @author ChangCheng Lu
  * @date 2023/8/21 14:11
@@ -10,8 +17,25 @@ package com.cclu.springframework.aop.framework;
  * JDK 动态代理
  * <p>
  */
-public class JdkDynamicAopProxy {
+public class JdkDynamicAopProxy implements AopProxy, InvocationHandler {
 
+    private final AdvisedSupport advised;
 
+    public JdkDynamicAopProxy(AdvisedSupport advised) {
+        this.advised = advised;
+    }
 
+    @Override
+    public Object getProxy() {
+        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), advised.getTargetSource().getTargetClass(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (advised.getMethodMatcher().matches(method, advised.getTargetSource().getTarget().getClass())) {
+            MethodInterceptor methodInterceptor = advised.getMethodInterceptor();
+            return methodInterceptor.invoke(new ReflectiveMethodInvocation(advised.getTargetSource().getTarget(), method, args));
+        }
+        return method.invoke(advised.getTargetSource().getTarget(), args);
+    }
 }
--- micro-spring-step-11/src/test/java/com/cclu/springframework/FrameworkTest.java
+++ micro-spring-step-11/src/test/java/com/cclu/springframework/FrameworkTest.java
@@ -1,7 +1,11 @@
 package com.cclu.springframework;
 
+import com.cclu.springframework.aop.MethodMatcher;
+import com.cclu.springframework.aop.aspectj.AspectJExpressionPointcut;
+import com.cclu.springframework.aop.framework.ReflectiveMethodInvocation;
 import com.cclu.springframework.bean.IUserService;
 import com.cclu.springframework.bean.UserService;
+import org.aopalliance.intercept.MethodInterceptor;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationHandler;
@@ -19,11 +23,32 @@ public class FrameworkTest {
         Object targetObj = new UserService();
         IUserService proxy = (IUserService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), targetObj.getClass().getInterfaces(), new InvocationHandler() {
 
+            MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* com.cclu.springframework.bean.IUserService.*(..))");
+
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                return null;
+                if (methodMatcher.matches(method, targetObj.getClass())) {
+                    // 方法拦截器
+                    MethodInterceptor methodInterceptor = invocation -> {
+                        long start = System.currentTimeMillis();
+                        try {
+                            return invocation.proceed();
+                        } finally {
+                            System.out.println("监控 - Begin By AOP");
+                            System.out.println("方法名称：" + invocation.getMethod().getName());
+                            System.out.println("方法耗时：" + (System.currentTimeMillis() - start) + "ms");
+                            System.out.println("监控 - End\r\n");
+                        }
+                    };
+                    // 反射调用
+                    return methodInterceptor.invoke(new ReflectiveMethodInvocation(targetObj, method, args));
+                }
+                return method.invoke(targetObj, args);
             }
         });
+
+        String result = proxy.queryUserInfo();
+        System.out.println("测试结果：" + result);
     }
 
 }
